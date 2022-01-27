3967f6cbb8e72dea89320d4dfe477bf39678a7b7
ErminSelimovic <92504422+ErminSelimovic@users.noreply.github.com>
2022-01-27 21:20:42 +0100

    Create Main.java

---
+++ Homework5/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        double a = 8;
+
+        double P = 6*(a*a);
+        double V = a*a*a;
+
+        System.out.println("Površina kocke iznosi "+ P + " cm, a zapremina kocke iznosi " + V + " cm.");
+
+    }
+}
