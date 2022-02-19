b6755d00312fc4eeeb2019ba135c29a476357a89
ErminSelimovic <92504422+ErminSelimovic@users.noreply.github.com>
2022-02-19 12:02:32 +0100

    Create Main.java

---
+++ Homework13/Main.java
@@ -0,0 +1,48 @@
+package com.company;
+
+import java.util.Scanner;
+import static java.lang.String.valueOf;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Unesite dva broja broja(dozvoljeno je unjeti double) ");
+        System.out.print("Num1=");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Num2=");
+        double num2 = scanner.nextDouble();
+
+        System.out.println("\nU nastavku sve aritmetičke operacije nad ova dva broja");
+        double sabiranje = num1 + num2;
+        double oduzimanje = num1 - num2;
+        double mnozenje = num1 * num2;
+        double dijeljenje = num1 / num2;
+        double modul = num1 % num2;
+
+        System.out.println("Zbir dva broja: " + sabiranje +
+                "\nOduzimanje dva broja: " + oduzimanje +
+                "\nMnoženje dva broja: " + mnozenje +
+                "\nDijeljenje dva broja: " + dijeljenje +
+                "\nModul dva broja: " + modul);
+
+        System.out.println("Najveći broj između svih rezultata je: " +
+                Math.max(Math.max(Math.max(sabiranje, oduzimanje), Math.max(mnozenje, dijeljenje)), modul)
+        );
+
+        System.out.print("\nUnesite jedan string: ");
+        scanner.nextLine();
+        String stringText = scanner.nextLine();
+        System.out.println("Dužina stringa je " + stringText.length() + " slova\n" +
+                "a korijen iz " + stringText.length() + " je " + Math.sqrt(stringText.length()));
+
+        System.out.print("\nUnesite jedan (long) broj: ");
+        long longNum = scanner.nextLong();
+        String longToString = valueOf(longNum);
+
+        System.out.println("Broj koji ste unjeli je: " + longNum +
+                "\na dužina ovog broja kao string je " + longToString.length());
+    }
+}
