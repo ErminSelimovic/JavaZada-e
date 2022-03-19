a3a488b019a1592ae0a7050070c713ce56a9b5ed
ErminSelimovic <92504422+ErminSelimovic@users.noreply.github.com>
2022-03-19 13:28:18 +0100

    Finalno

--- Homework20/Main.java
+++ Homework20/Main.java
@@ -21,8 +21,7 @@ public class Main {
             System.out.print("Please enter song for " + singer + ": ");
             song = scan.nextLine();
             songsList.put(singer, song);
-
-
+            
             do {
                 System.out.println("*******CHOICE******");
                 System.out.println("1 - For add new singer and song");
@@ -37,61 +36,32 @@ public class Main {
                 } else {
                     System.out.println("That is wrong enter, please choice again");
                 }
-
                 scan.nextLine();
-
             } while (choice < 0 || choice > 1);
 
 
         }
-
+        int playRepeat;
         for (String sngList : songsList.keySet()) {
-            int playRepeat;
+
             System.out.println("Next song: " + sngList + " - " + songsList.get(sngList));
             System.out.print("Play (1) or Next (0)\n");
 
             do {
-
                 playRepeat = scan.nextInt();
 
-                if (playRepeat == 0){
-                    break;
-                }
-
-                if (playRepeat == 1){
-                    
-                    while (playRepeat == 1) {
+                switch (playRepeat) {
+                    case 0:
+                        break;
+                    case 1:
                         System.out.printf("♫♫ Now Playing ♫♫ %s-%s ", sngList, songsList.get(sngList));
                         System.out.println("\nFor repeat enter (1), for Next enter (0)");
-                        playRepeat = scan.nextInt();
-
-                        if (playRepeat != 1 && playRepeat > 0){
-                            System.out.println("Wrong enter, please choice again");
-                        }
-                    }
-
+                        break;
+                    default:
+                        System.out.println("Wrong enter");
+                        System.out.print("Play (1) or Next (0)\n");
                 }
-
-
-                if (playRepeat > 1 || playRepeat < 0) {
-                    System.out.println("Wrong enter");
-                    System.out.println("Next song: " + sngList + " - " + songsList.get(sngList));
-                    System.out.print("Play (1) or Next (0)\n");
-                    while (playRepeat != 1) {
-                        playRepeat = scan.nextInt();
-                        if (playRepeat != 1){
-                            System.out.println("Wrong enter");
-                            System.out.println("Next song: " + sngList + " - " + songsList.get(sngList));
-                            System.out.print("Play (1) or Next (0)\n");
-                            playRepeat = scan.nextInt();
-                        }
-                    }
-                }
-
-
-
-            } while (playRepeat == 0);
-
+            } while (playRepeat != 0);
 
         }
 
