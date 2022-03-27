a831e4254508ff2c96173ee46aac8af5e253cf3f
ErminSelimovic <92504422+ErminSelimovic@users.noreply.github.com>
2022-03-27 21:34:27 +0200

    Update Main.java

--- Homework22/Main.java
+++ Homework22/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
 
-        SmartWatch sat1 = new SmartWatch(1,"Watch GT2","Crna","Huawei",false);
-
+        SmartWatch sat1 = new SmartWatch(1,"Watch GT2","Crna","Huawei",true);
+        System.out.printf("Vaš uređaj je %s branda %s boja %s i trenutno %s",sat1.imeSata,sat1.brand, sat1.boja,
+                ((sat1.bluetoothConnect) ? "je konektovan" : "nije konektovan"));
         sat1.isConnected();
         sat1.showTime();
         sat1.showDate();
